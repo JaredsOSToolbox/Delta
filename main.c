ddb6206a4c6ce2601151242e7922aff1b6be9aeb
JaredDyreson <jared.dyreson@gmail.com>
2020-04-01 23:37:34 -0700

    they now print side by side perfectly

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "file_struct.h"
+#include <string.h>
 
 int main(int argc, const char* argv[]){
   struct file_* f_a = file_constructor("inputs/left.txt");
@@ -7,7 +8,7 @@ int main(int argc, const char* argv[]){
   /*printf("%s\n", f_b->file_path);*/
   /*if(__eq__(f_a, f_b)){*/
     /*printf("they are the same!\n");*/
-  /*}*/
+    /*}*/
   /*else{*/
     /*printf("they are not the same!\n");*/
   /*}*/
